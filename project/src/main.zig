00c999d147d7c0030055351fe50f366d539b5651
bereskristof <113383819+bereskristof@users.noreply.github.com>
2025-04-08 13:04:40 +0200

    P3 support

--- project/src/main.zig
+++ project/src/main.zig
@@ -1,15 +1,26 @@
 const std = @import("std");
 const cl = @import("cl_binding.zig");
-const Image = @import("netpbm.zig");
 const args = @import("argparse.zig");
-const buildConfig = @import("build_config");
+const build_config = @import("build_config");
+
+const Image = @import("netpbm.zig");
 
 const exit_success = 0;
 const exit_failure = 1;
 const exit_complete_failure = 2;
 const help_text = @embedFile("help_text.txt");
-const name = buildConfig.name;
-const version = buildConfig.version;
+const name = build_config.name;
+const version = build_config.version;
+
+const HandlingError = error{
+    FileNotFound,
+    AccessDenied,
+    SourceIsMissing,
+    OutOfMemory,
+    FileIsDamaged,
+    Unsupported,
+    UnknownError,
+};
 
 pub fn main() u8 {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -29,10 +40,58 @@ pub fn main() u8 {
     switch (run_config.tool) {
         .Default, .Help => stdout.print(help_text, .{name}) catch return exit_complete_failure,
         .Version => stdout.print("{s}\n", .{version}) catch return exit_complete_failure,
-        else => _ = stdout.write("Todo") catch return exit_complete_failure,
+        else => _ = handleInputs(run_config) catch |err| switch (err) {
+            error.FileNotFound => return errorExit("Error: File not found!\n"),
+            error.AccessDenied => return errorExit("Error: Access denied!\n"),
+            error.SourceIsMissing => return errorExit("Error: No source provided!\n"),
+            error.OutOfMemory => return errorExit("Error: Not enough memory available!\n"),
+            error.FileIsDamaged => return errorExit("Error: File data is corrupted!\n"),
+            error.Unsupported => return errorExit("Error: File type is unsupported!\n"),
+            error.UnknownError => return errorExit("Error: Unknown!\n"),
+        },
     }
 
     return exit_success;
+}
+
+fn handleInputs(properies: args.Properties) HandlingError!void {
+    switch (properies.source) {
+        .files => {
+            var i: usize = 0;
+            while (properies.getFile(i)) |file_name| : (i += 1) {
+                handleFile(file_name) catch |err| if (!properies.silent) {
+                    return err;
+                } else {
+                    return;
+                };
+            }
+        },
+        .stdin => {
+            return; // TODO: Replace with implementation
+        },
+        .none => return HandlingError.SourceIsMissing,
+    }
+}
+
+fn handleFile(file_name: []const u8) HandlingError!void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    var image = Image.initFromFile(allocator, file_name) catch |err| switch (err) {
+        error.FileNotFound => return HandlingError.FileNotFound,
+        error.AccessDenied => return HandlingError.AccessDenied,
+        error.Unsupported => return HandlingError.Unsupported,
+        error.WrongMagicNumber => return HandlingError.Unsupported,
+        error.CorruptedHeaderData => return HandlingError.FileIsDamaged,
+        error.OutOfMemory => return HandlingError.OutOfMemory,
+        error.UnreadableString => return HandlingError.FileIsDamaged,
+        error.DamagedData => return HandlingError.FileIsDamaged,
+        else => return HandlingError.UnknownError,
+    };
+    defer image.deinit();
+
+    image.print() catch {};
 }
 
 fn errorExit(comptime msg: []const u8) u8 {
--- project/src/netpbm.zig
+++ project/src/netpbm.zig
@@ -1,12 +1,14 @@
 const std = @import("std");
 
-const Self = @This();
+const This = @This();
 
 const UnsupportedFileError = error{
     Unsupported,
     WrongMagicNumber,
     CorruptedHeaderData,
     OutOfMemory,
+    UnreadableString,
+    DamagedData,
 };
 
 const BitWidth = enum(u2) {
@@ -16,6 +18,7 @@ const BitWidth = enum(u2) {
 
 const ImageInfo = struct {
     size: usize,
+    byte_size: usize,
     width: u64,
     height: u64,
     white_value: u16,
@@ -27,35 +30,62 @@ data: []const u8,
 
 alloc: std.mem.Allocator,
 
-pub fn initFromFile(a: std.mem.Allocator, file_path: []const u8) !Self {
+pub fn initFromFile(a: std.mem.Allocator, file_path: []const u8) !This {
     const cwd = std.fs.cwd();
     const file = try cwd.openFile(file_path, .{});
     defer file.close();
 
     const file_data = try file.readToEndAlloc(a, 1 << 30);
-    return try init(file_data);
+    defer a.free(file_data);
+    return try init(a, file_data);
 }
 
-pub fn init(a: std.mem.Allocator, file_data: []const u8) UnsupportedFileError!Self {
+pub fn init(a: std.mem.Allocator, file_data: []const u8) UnsupportedFileError!This {
     if (file_data.len < 3 or file_data[0] != 'P' or !std.ascii.isWhitespace(file_data[2])) {
         return UnsupportedFileError.WrongMagicNumber;
     }
     return switch (file_data[1]) {
+        '3' => initP3(a, file_data),
         '6' => initP6(a, file_data),
-        '1'...'5', '7' => UnsupportedFileError.Unsupported,
+        '1', '2', '4', '5', '7' => UnsupportedFileError.Unsupported,
         else => return UnsupportedFileError.WrongMagicNumber,
     };
 }
 
-fn initP6(a: std.mem.Allocator, file_data: []const u8) UnsupportedFileError!Self {
+fn initP3(a: std.mem.Allocator, file_data: []const u8) UnsupportedFileError!This {
     var data_start: usize = 0;
     const image_info = try readP3Header(&data_start, file_data);
 
-    std.debug.print("{}\n", .{@intFromEnum(image_info.bit_width)});
-    const rgb_data = a.alloc(u8, 3 * image_info.size * @intFromEnum(image_info.bit_width)) catch return UnsupportedFileError.OutOfMemory;
+    const rgb_data = a.alloc(u8, image_info.byte_size) catch return UnsupportedFileError.OutOfMemory;
+    errdefer a.free(rgb_data);
+    var rgb_i: usize = 0;
+    var iterator = HeaderIterator.init(file_data[data_start..]);
+    while (iterator.next()) |word| {
+        const value = std.fmt.parseUnsigned(u8, word, 10) catch return UnsupportedFileError.UnreadableString;
+        rgb_data[rgb_i] = value;
+        rgb_i += 1;
+    }
+    if (rgb_i != image_info.byte_size) return UnsupportedFileError.DamagedData;
+
+    return .{
+        .image_info = image_info,
+        .data = rgb_data,
+        .alloc = a,
+    };
+}
+
+fn initP6(a: std.mem.Allocator, file_data: []const u8) UnsupportedFileError!This {
+    var data_start: usize = 0;
+    const image_info = try readP3Header(&data_start, file_data);
+
+    const rgb_data = a.alloc(u8, image_info.byte_size) catch return UnsupportedFileError.OutOfMemory;
+    errdefer a.free(rgb_data);
+    var read_data: usize = 0;
     for (file_data[data_start..], 0..) |byte, i| {
         rgb_data[i] = byte;
+        read_data = i;
     }
+    if (read_data != image_info.byte_size - 1) return UnsupportedFileError.DamagedData;
 
     return .{
         .image_info = image_info,
@@ -78,6 +108,7 @@ fn readP3Header(header_size: *usize, file_data: []const u8) UnsupportedFileError
 
     return .{
         .size = width * height,
+        .byte_size = 3 * (width * height) * (if (max_color_value >= 256) @as(usize, 2) else @as(usize, 1)),
         .width = width,
         .height = height,
         .white_value = max_color_value,
@@ -85,7 +116,7 @@ fn readP3Header(header_size: *usize, file_data: []const u8) UnsupportedFileError
     };
 }
 
-pub fn print(self: Self) !void {
+pub fn print(self: This) !void {
     const stderr = std.io.getStdErr();
     const out = stderr.writer();
     for (self.data, 0..) |byte, i| {
@@ -98,7 +129,7 @@ pub fn print(self: Self) !void {
     }
 }
 
-pub fn deinit(self: *Self) void {
+pub fn deinit(self: *This) void {
     self.alloc.free(self.data);
 }
 
